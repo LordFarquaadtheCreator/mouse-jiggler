13eb9f51d2dbbdfd88913bd4bccc8d06750eef32
LordFarquaadtheCreator <fahadfaruqi1@gmail.com>
2024-11-29 15:31:25 -0500

    jiggler sleeps one second per jiggle

--- Program.cs
+++ Program.cs
@@ -9,20 +9,22 @@ namespace mouseJiggler
         private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
 
         private const int MOUSEEVENTF_MOVE = 0x0001;
-        private static int[] MovementArray = { 0x0002, 0x0004, 0x0008, 0x0010 };
+        private static int[] MOVEMENTARRAY = { 0x0002, 0x0004, 0x0008, 0x0010 };
+        private static int ONESECOND = (Int32) Math.Pow(10, 3);
 
         static void Main(string[] args)
         {
             Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelEvent);
-            
+
             while (true)
             {
-                // sleep for one second
+                System.Threading.Thread.Sleep(ONESECOND);
+
                 var rand = new Random();
                 int direction1 = rand.Next(0, 3);
                 int direction2 = rand.Next(0, 3);
 
-                mouse_event(MOUSEEVENTF_MOVE, MovementArray[direction1], MovementArray[direction2], 0, 0);
+                mouse_event(MOUSEEVENTF_MOVE, MOVEMENTARRAY[direction1], MOVEMENTARRAY[direction2], 0, 0);
             }
         }
 
