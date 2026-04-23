c840e2260f9d02ad418dc79749787be56e155b9f
LordFarquaadtheCreator <fahadfaruqi1@gmail.com>
2026-04-23 17:22:40 -0400

    feat: add error handling, logger output, coordinate logging

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ static inline CGEventRef nullEvent() { return NULL; }
 import "C"
 
 import (
-	"fmt"
+	"log"
 	"math/rand"
 	"os"
 	"os/signal"
@@ -23,11 +23,11 @@ func main() {
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
-		fmt.Println("\nExiting...")
+		log.Println("Exiting...")
 		os.Exit(0)
 	}()
 
-	fmt.Println("Mouse jiggler running. Ctrl+C to exit.")
+	log.Println("Mouse jiggler running. Ctrl+C to exit.")
 
 	movements := []int{2, 4, 8, 16}
 	ticker := time.NewTicker(1 * time.Second)
@@ -36,26 +36,30 @@ func main() {
 	for range ticker.C {
 		dx := movements[rand.Intn(4)]
 		dy := movements[rand.Intn(4)]
-		jiggle(dx, dy)
+		if err := jiggle(dx, dy); err != nil {
+			log.Printf("jiggle failed: %v", err)
+		}
 	}
 }
 
-func jiggle(dx, dy int) {
+func jiggle(dx, dy int) error {
 	event := C.CGEventCreate(C.nullEventSource())
 	if event == C.nullEvent() {
-		return
+		return log.Output(2, "CGEventCreate returned nil")
 	}
 	defer C.CFRelease(C.CFTypeRef(event))
 
 	pt := C.CGEventGetLocation(event)
-	pt.x += C.double(dx)
-	pt.y += C.double(dy)
+	newX := pt.x + C.double(dx)
+	newY := pt.y + C.double(dy)
 
-	moveEvent := C.CGEventCreateMouseEvent(C.nullEventSource(), C.kCGEventMouseMoved, pt, 0)
+	moveEvent := C.CGEventCreateMouseEvent(C.nullEventSource(), C.kCGEventMouseMoved, C.CGPoint{x: newX, y: newY}, 0)
 	if moveEvent == C.nullEvent() {
-		return
+		return log.Output(2, "CGEventCreateMouseEvent returned nil")
 	}
 	defer C.CFRelease(C.CFTypeRef(moveEvent))
 
 	C.CGEventPost(C.kCGHIDEventTap, moveEvent)
+	log.Printf("moved mouse to (%.0f, %.0f)", newX, newY)
+	return nil
 }
